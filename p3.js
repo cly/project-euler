6d8f8ba3400ef6639bcd49d9b0b0e5e37e91377e
Charlie Liang Yuan <charlie.liang.yuan@gmail.com>
2011-06-05 12:57:38 -0700

    p3

--- p3.js
+++ p3.js
@@ -1,19 +1,29 @@
-var target = 600851475143,
-    currTarget = target,
-    i = 2,
-    factors = [];
+//The prime factors of 13195 are 5, 7, 13 and 29.
+//
+//What is the largest prime factor of the number 600851475143 ?
+
+var target = 600851475143;
 
 //600 billion, try all numbers = fail
 //try all numbers but divide by that number maybe ok?
-while(currTarget > 1){
-  if(currTarget % i === 0){
-    currTarget /= i;
-    factors.push(i);
+function getFactors(currTarget){
+  var i = 2,
+      factors = [];
+
+  while(currTarget > 1){
+    if(currTarget % i === 0){
+      currTarget /= i;
+      factors.push(i);
+    }
+    else {
+      ++i;
+    }
   }
-  else {
-    ++i;
-  }
+  return factors;
 }
+
+getFactors(target);
+console.log(target);
 console.log(currTarget);
 console.log(factors);
 console.log(factors.pop());
