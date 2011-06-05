bb72d07c1c333a5f5daf6650ecc38406770deeb1
Charlie Liang Yuan <charlie.liang.yuan@gmail.com>
2011-06-05 13:24:00 -0700

    solution

--- p3.js
+++ p3.js
@@ -2,28 +2,14 @@
 //
 //What is the largest prime factor of the number 600851475143 ?
 
-var target = 600851475143;
+var common = require('./common.js');
+
+var target = 600851475143,
+    factors = [];
 
 //600 billion, try all numbers = fail
-//try all numbers but divide by that number maybe ok?
-function getFactors(currTarget){
-  var i = 2,
-      factors = [];
-
-  while(currTarget > 1){
-    if(currTarget % i === 0){
-      currTarget /= i;
-      factors.push(i);
-    }
-    else {
-      ++i;
-    }
-  }
-  return factors;
-}
-
-getFactors(target);
+//try all numbers but divide by that number maybe ok? - basically find factors
+factors = common.getFactors(target);
 console.log(target);
-console.log(currTarget);
 console.log(factors);
 console.log(factors.pop());
---
+++ p5.js
@@ -0,0 +1,43 @@
+//2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+//
+//What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+//the prime factorization of 2520 can be combined to compute all the numbers between 1 and 10
+//answer: 232792560
+
+var common = require('./common.js');
+
+
+(function(){
+  var MIN = 1,
+      MAX = 20,
+      factorCounts = {},
+      factors,
+      factor,
+      tempFactorCounts;
+  for(var i = MIN, ii = MAX; i <= ii; --ii){
+    factors = common.getFactors(ii);
+    tempFactorCounts = common.getFactorCounts(factors);
+
+    for(factor in tempFactorCounts){
+      if(tempFactorCounts.hasOwnProperty(factor)){
+        if(typeof factorCounts[factor] === 'undefined' ||
+           factorCounts[factor] < tempFactorCounts[factor]){
+          factorCounts[factor] = tempFactorCounts[factor];
+        }
+      }
+    }
+    console.log(tempFactorCounts);
+  }
+  console.log(factorCounts);
+
+  var result = 1;
+  for(factor in factorCounts){
+    if(factorCounts.hasOwnProperty(factor)){
+      result *= Math.pow(factor,factorCounts[factor]);
+    }
+  }
+  console.log(result);
+}());
+
+
